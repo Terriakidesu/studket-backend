e577beda3739e752e9e4c102e5667f760ef57565
Terriakidesu <trra53@gmail.com>
2026-03-10 20:15:17 +0800

    chore: remove obsolete listing api package

--- app/api/listing/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from . import auth, listing
-
-router = APIRouter(prefix="/v1")
-
-router.include_router(listing.router)
-router.include_router(auth.router)
--- app/api/listing/listing.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, Request
-
-router = APIRouter(prefix="/listings")
-
-@router.get("/")
-async def products_list(request: Request):
-    return []
